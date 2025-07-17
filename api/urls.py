0a6e826d927ea298809adf9b0e56074649ea7e7e
nomena-sombiniaina-alfa <andriamifidisoaalfanomena@gmail.com>
2025-07-17 12:13:49 +0300

    Hook up login + token refresh routes from simplejwt

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import views
 
 urlpatterns = [
     path('auth/register/', views.RegisterView.as_view(), name='register'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='login'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='refresh'),
     path('me/', views.MeView.as_view(), name='me'),
 ]
